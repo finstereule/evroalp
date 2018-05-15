af8382b825a488a349da3c1fb0593011f964d889
Halyna Chemerys <chemerys.halyna@gmail.com>
2018-05-15 19:41:45 +0300

    main changes on "materials" page

--- html/js/custom.js
+++ html/js/custom.js
@@ -1,9 +1,94 @@
-// Hide CTA when clicking on "MENU" button
-document.getElementById("navbar-toggler").addEventListener("click", function(){    
-	    var x = document.getElementById("center-block");
-    if (x.style.display === "none") {
-        x.style.display = "block";
+// Hide header when clicking on "MENU" button
+
+$(document).ready(function(){
+	$('#navbar-toggler').click(function(){
+	var mainHeader = $("#center-block")[0];
+    if (mainHeader.style.display === "none") {
+        mainHeader.style.display = "block";
     } else {
-        x.style.display = "none";
+        mainHeader.style.display = "none";
     }
+
+    $(".toggle-icon")[0].classList.toggle("anim");
+	});
+
+
+	$('.materialsCarousel').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 2,
+  arrows: false,
+  draggable: false,
+   autoplay: true,
+  autoplaySpeed: 3000,
+  responsive: [
+     {
+      breakpoint: 1900,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    }
+  ]
 });
+
+	$('.portfolioCarousel').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 2,
+  arrows: false,
+  draggable: false,
+   autoplay: true,
+  autoplaySpeed: 3000,
+  responsive: [
+     {
+      breakpoint: 1900,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 2
+      }
+    },
+      {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
+});
+
